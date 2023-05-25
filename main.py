114c4538e7c087894bb5aadbf471164654ce9077
2yunsu <2yunsu@snu.ac.kr>
2023-05-25 15:53:59 +0900

    lower compare code added

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ random.seed(random_seed)  # random
 
 #parameter
 num_epochs = 100
-num_iter = 1000
+num_iter = 100
+num_extract = 10
 
 # Define the input and target
 x = torch.linspace(-2, 2, 100).view(-1,1)
@@ -33,7 +34,7 @@ theta_2 = torch.linspace(0, 1 * np.pi, num_epochs).view(-1,1)
 # Train the model
 mse_losses = []
 mse_weights_list = []
-mse_bias_list = []
+# mse_bias_list = []
 converged_epochs = []
 
 for i in tqdm(range(num_iter)):
@@ -61,7 +62,7 @@ for i in tqdm(range(num_iter)):
         # Backward pass
         loss.backward()
         crr_weights_list.append(weights.item())
-        crr_bias_list.append(bias.item())
+        # crr_bias_list.append(bias.item())
         optimizer.step()
 
         # Store the loss
@@ -73,13 +74,13 @@ for i in tqdm(range(num_iter)):
                 gradient = losses[-1] - losses[-2] # 현재 epoch과 이전 epoch의 loss 값 차이
                 if abs(gradient) < 0.07: # 기울기가 0에 가까워질 때마다 converged_epochs 리스트에 epoch 값을 추가
                     converged_epochs.append(epoch)
-                if epoch == num_epochs-1: # 한 epoch 끝까지 수렴 안 하면 마지막 값을 수렴점으로 지정
+                if epoch == num_epochs - 1: # 한 epoch 끝까지 수렴 안 하면 마지막 값을 수렴점으로 지정
                     converged_epochs.append(epoch)
 
     # Store the losses
     mse_losses.append(losses)
     mse_weights_list.append(crr_weights_list)
-    mse_bias_list.append(crr_bias_list)
+    # mse_bias_list.append(crr_bias_list)
 
 gradients_list = []
 for i in range(len(mse_weights_list)):
@@ -112,33 +113,41 @@ for i in range(len(mse_weights_list)):
         gradients_diff_mean = gradients_diff_mean_1
     gradients_list.append(gradients_diff_mean)
 
-    # #compare gradient graph
-    # plt.plot(x_1, cycloid_graph_1_1, label="Cycloid", color="Orange")
-    # plt.plot(x_2, cycloid_graph_2_1, color="Orange")
-    # plt.plot(mse_weights_list[i], mse_losses[i], label="MSE Loss Function")
-    # plt.xlabel("weight")
-    # plt.ylabel("Loss")
-    # plt.legend()
-    # # plt.show()
+    #compare gradient graph
+    plt.plot(x_1, cycloid_graph_1_1, label="Cycloid", color="Orange")
+    plt.plot(x_2, cycloid_graph_2_1, color="Orange")
+    plt.plot(mse_weights_list[i], mse_losses[i], label="MSE Loss Function")
+    plt.xlabel("weight")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
     # plt.savefig('fig1.png')
-    # print()
+    print()
 
 min_idx = np.argmin(gradients_list)
 mse_losses_mean_min = mse_losses[min_idx]
 mse_losses_mean = np.mean(mse_losses, axis=0)
 
-min_idx_10 = np.argsort(gradients_list)[:(10)] #상위 5% 추출하여 비교
+min_idx_10 = np.argsort(gradients_list)[:num_extract] #상위 n개 추출하여 비교
+
+sorted_indices = sorted(range(len(mse_losses)), key=lambda i: mse_losses[i][0]) # 0번째 원소를 기준으로 리스트를 정렬한 후 인덱스를 정렬
+init_loss_min_idx_10 = sorted_indices[:num_extract]  # 상위 n개 인덱스 추출
 
 converged_10_list = []
 for i in range(len(min_idx_10)):
     converged_10_list.append(converged_epochs[min_idx_10[i]])
 
+init_converged_10_list = []
+for i in range(len(init_loss_min_idx_10)):
+    init_converged_10_list.append(converged_epochs[init_loss_min_idx_10[i]])
+
 #when converge
-print("Top Cycloid Loss function Converge at: ", converged_epochs[min_idx])
-print("Top 5% Cycloid Loss function Converge Mean at: ", converged_10_list)
+# print("Top Cycloid Loss function Converge at: ", converged_epochs[min_idx])
 print("Top 5% Cycloid Loss function Converge Mean at: ", np.mean(converged_10_list))
+print("Top 5% low initial loss Mean: ", np.mean(init_converged_10_list))
 print("Converge Mean: ", np.mean(converged_epochs))
 print("Top 5% Cycloid Loss Varience: ", np.var(converged_10_list))
+print("Top 5% low initial Loss Varience: ", np.var(init_converged_10_list))
 print("MSE Loss Varience: ", np.var(converged_epochs))
 
 # Plot the losses
@@ -152,10 +161,13 @@ for i in range(len(mse_losses)):
 for i in range(len(converged_10_list)):
     plt.plot(mse_losses[min_idx_10[i]], color='tab:cyan')
 
+for i in range(len(init_converged_10_list)):
+    plt.plot(mse_losses[init_loss_min_idx_10[i]], color='tab:orange')
+
 plt.plot(mse_losses_mean, label="Mean of MSE", color='red')
 
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend()
-plt.savefig("fig2.png")
-# plt.show()
+# plt.savefig("fig2.png")
+plt.show()
